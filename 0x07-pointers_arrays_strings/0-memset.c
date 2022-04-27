bc876465447160586b1e66269a4edf4795cdf21c
Chinaemerem <alionyechinaemerem@gmail.com>
2022-04-27 01:20:14 -0700

    mod

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -7,11 +7,10 @@
  * @n: bytes
  * Return: Always 0 (Success)
  */
-	char *_memset(char *s, char b, unsigned int n)
-	{
-		unsigned int i;
-
-		for (i = 0; i < n; i++)
-			s[i] = b;
-		return (s);
-	}
+char *_memset(char *s, char b, unsigned int n)
+{
+  unsigned int i;
+  for (i = 0; i < n; i++)
+    s[i] = b;
+  return (s);
+}
