04981c8ffb48f46e1ec8b9d38ba2fc4650b29f39
Chinaemerem <alionyechinaemerem@gmail.com>
2022-04-27 22:25:08 +0100

    changed styling

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,22 +1,4 @@
 #include "main.h"
-<<<<<<< HEAD
-
-/**
- * _memset - Entry point
- * @s: pointed destination
- * @b: constant byte
- * @n: bytes
- * Return: Always 0 (Success)
- */
-	char *_memset(char *s, char b, unsigned int n)
-	{
-		unsigned int i;
-
-		for (i = 0; i < n; i++)
-			s[i] = b;
-		return (s);
-	}
-=======
 /**
  * _memset - fills memory with a constant byte,
  * @s: memory area.
@@ -33,4 +15,3 @@ for (i = 0; i < n; i++)
 
 return (s);
 }
->>>>>>> e1ad1101524b00818f034d5ecc8aba6d313737b7
--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,6 +1,4 @@
-<<<<<<< HEAD
 #include "main.h"
-
 /**
  * _memcpy - Entry point
  * @dest: input
@@ -10,31 +8,9 @@
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	unsigned int i;
+unsigned int i;
+for (i = 0; i < n; i++)
+dest[i] = src[i];
 
-	for (i = 0; i < n; i++)
-		dest[i] = src[i];
-
-	return (dest);
+return (dest);
 }
-=======
-#include "main.h"
-
-/**
- * _memcpy - Entry point
- * @dest: input
- * @src: input
- * @n: input
- * Return: Always 0 (Success)
- */
-
-char *_memcpy(char *dest, char *src, unsigned int n)
-{
-	unsigned int i;
-
-	for (i = 0; i < n; i++)
-		dest[i] = src[i];
-
-	return (dest);
-}
->>>>>>> e1ad1101524b00818f034d5ecc8aba6d313737b7
