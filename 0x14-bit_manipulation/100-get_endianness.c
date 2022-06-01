9b4a6a677050ba3f497878accc28d9a9c1aba3ff
Chinaemerem <alionyechinaemerem@gmail.com>
2022-06-01 06:13:49 +0100

    project_updates

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -8,11 +8,11 @@
 
 int get_endianness(void)
 {
-	unsigned int test = 1;
-	char *endian = (char *)&test;
+unsigned int test = 1;
+char *endian = (char *)&test;
 
-	if (*endian)
-		return (1);
-	return (0);
+if (*endian)
+return (1);
+return (0);
 
 }
--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -10,14 +10,14 @@
 
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	unsigned long int flipped = n ^ m;
-	int count = 0;
+unsigned long int flipped = n ^ m;
+int count = 0;
 
-	while (flipped)
-	{
-		if (flipped & 1)
-			count++;
-		flipped >>= 1;
-	}
-	return (count);
+while (flipped)
+{
+if (flipped & 1)
+count++;
+flipped >>= 1;
+}
+return (count);
 }
